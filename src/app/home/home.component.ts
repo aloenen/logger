8bcebcdde2082aaeee31c68db674ef1aaf168f92
Andrew <andrewloenen97@gmail.com>
2019-12-01 21:56:21 -0600

    carousel added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  chart = [];
+
   constructor() { }
 
   ngOnInit() {
+
+    this.chart = new Chart('canvas', {
+      type: 'line',
+      data: {
+        labels: "weatherDates",
+        datasets: [
+          { 
+            data: [3,3,3,3,3,3,3,4],
+            borderColor: "#3cba9f",
+            fill: false
+          },
+          { 
+            data: [2,3,4,5,5,6,7,7],
+            borderColor: "#ffcc00",
+            fill: false
+          },
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: true
+          }],
+          yAxes: [{
+            display: true
+          }],
+        }
+      }
+    });
   }
 
 }
+
+
+ 
+
